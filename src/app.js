1555b60d18f5c5b10975468d844add9ff72e8b05
bohdan-saiko <bohdan.saiko.m@gmail.com>
2026-02-27 22:50:13 +0200

    init commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server start on http://localhost:${PORT}`);
+})
