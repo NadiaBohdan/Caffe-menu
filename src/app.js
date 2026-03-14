dbac76f8bdd1cb3c8143b7cf1e28d07bc88161b0
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-14 12:38:50 +0200

    chore(routes): devide routes ssr/api

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser())
 
-app.use('/', apiRouter);
+app.use('/api', apiRouter);
 
 app.get('/ad', (req, res) => {
   res.json({message: "Some message"});
