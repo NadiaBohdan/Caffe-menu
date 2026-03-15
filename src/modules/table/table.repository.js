88065825ba5e98d040846f7b9d5be32df65dab25
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-15 12:06:12 +0200

    feat(cafeTable): implement repository layer

---
+++ src/modules/table/table.repository.js
@@ -0,0 +1,55 @@
+import prisma from "#configs/prisma.js"
+
+export const tableRepository = {
+    /**
+     * @param {Object} data
+     * @param {number} data.tableNumber
+     */
+
+    async create({ tableNumber }) {
+        return await prisma.cafeTable.create({
+            data: { tableNumber }
+        })
+    },
+
+    async getAll() {
+        return await prisma.cafeTable.findMany()
+    },
+
+    /**
+     * @param {Object}  data
+     * @param {number}  data.tableNumber
+     * @param {boolean} data.isAvailable
+     * @param {number}  data.id 
+     */
+
+    async update({id, ...data}) {
+        return await prisma.cafeTable.update({
+            where: { id },
+            data: data
+        })
+    },
+
+    /**
+     * @param {Object}  data
+     * @param {boolean} data.status
+     * @param {number}  data.id 
+     */
+
+    async setStatus({ id, status }) {
+        return await prisma.cafeTable.update({
+            where: { id },
+            data: { isAvailable: status }
+        })
+    },
+
+    /**
+     * @param {number} id 
+     */
+
+    async delete(id) {
+        return await prisma.cafeTable.delete({
+            where: { id }
+        })
+    }
+}
