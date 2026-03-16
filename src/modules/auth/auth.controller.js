1787027a43e763185d46bf836a4c34775fa3d245
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-16 20:45:35 +0200

    refactor(controllers): remove try/catch

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,42 +1,33 @@
-import { authService} from "./auth.service.js"
+import { authService } from "./auth.service.js"
 
 export const authController = {
-    async register(req, res, next) {
-        try {
-            const { registerData } = req.body;
+    async register(req, res) {
+        const { registerData } = req.body;
 
-            const token = await authService.registerUser(registerData);
+        const token = await authService.registerUser(registerData);
 
-            res.cookie('accessToken', token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'Strict',
-                maxAge: 1000 * 60 * 60 * 24
-            })
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Strict',
+            maxAge: 1000 * 60 * 60 * 24
+        })
 
-            res.sendStatus(201);
-        } catch(err) {
-            next(err);
-        }
+        res.sendStatus(201);
     },
 
-    async login(req, res, next) {
-        try {
-            const { loginData } = req.body;
+    async login(req, res) {
+        const { loginData } = req.body;
 
-            const token = await authService.loginUser(loginData);
+        const token = await authService.loginUser(loginData);
 
-            res.cookie('accessToken', token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'Strict',
-                maxAge: 1000 * 60 * 60 * 24
-            })
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Strict',
+            maxAge: 1000 * 60 * 60 * 24
+        })
 
-            res.sendStatus(200);
-        } catch(err) {
-            next(err);
-        }
-        
+        res.sendStatus(200);
     }
 }
--- src/modules/table/table.controller.js
+++ src/modules/table/table.controller.js
@@ -1,49 +1,33 @@
 import { tableService } from "./table.service.js";
 
 export const tableController = {
-    async addTable(req, res, next) {
-        try {
-            const { tableNumber } = req.body.data;
+    async addTable(req, res) {
+        const { tableNumber } = req.body.data;
 
-            const table = await tableService.addTable({ tableNumber });
+        const table = await tableService.addTable({ tableNumber });
 
-            res.status(201).json({ message: "Table was successfully added", table });
-        } catch(err) {
-            next(err);
-        }
+        res.status(201).json({ message: "Table was successfully added", table });
     },
 
-    async updateTable(req, res, next) {
-        try {
-            const { id } = req.params;
-            const { tableNumber, isAvailable } = req.body.data;
+    async updateTable(req, res) {
+        const { id } = req.params;
+        const { tableNumber, isAvailable } = req.body.data;
 
-            const table = await tableService.updateTable({ id, tableNumber, isAvailable });
+        const table = await tableService.updateTable({ id, tableNumber, isAvailable });
 
-            res.status(200).json({ message: "Table was successfully updated", table });
-        } catch(err) {
-            next(err);
-        }
+        res.status(200).json({ message: "Table was successfully updated", table });
     },
 
-    async deleteTable(req, res, next) {
-        try {
-            const { id } = req.params;
+    async deleteTable(req, res) {
+        const { id } = req.params;
 
-            await tableService.deleteTable(id);
+        await tableService.deleteTable(id);
 
-            res.sendStatus(204);
-        } catch(err) {
-            next(err);
-        }
+        res.sendStatus(204);
     },
 
-    async getAllTables(req, res, next) {
-        try {
-            const tables = await tableService.getAll()
-            res.status(200).json({ tables });
-        } catch(err) {
-            next(err);
-        }
+    async getAllTables(req, res) {
+        const tables = await tableService.getAll()
+        res.status(200).json({ tables });
     }
 }
