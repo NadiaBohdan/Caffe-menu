c7c101066c73b642353c0e69395b6abee7eea293
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-17 08:41:40 +0200

    feat(error-handler): implement ZodErrors processing

--- src/middlwares/error.middlware.js
+++ src/middlwares/error.middlware.js
@@ -1,13 +1,25 @@
 import { ApiError } from "#utils/error.util";
+import { ZodError } from "zod";
 
 export const errorHandler = (err, req, res, next) => {
     if(err instanceof ApiError || err.name === 'ApiError') {
-        return res.status(err.status).json({
+        return res.status(err.status || 500).json({
             success: false,
             message: err.message
         })
     }
 
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            success: false,
+            message: err.issues[0]?.message || "Validation failed",
+            details: err.issues.map(e => ({
+                field: e.path.join('.'),
+                message: e.message
+            }))
+        });
+    }
+   
     console.error(err)
     return res.status(500).json({
         success: false,
