26d6075ef6a920f569dfe5bdab2774f9860f7436
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-20 19:16:01 +0200

    fix(routes): add routes to index.js

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 const router = express.Router();
 
 import authRoutes from "#auth/auth.routes";
-import tableRoutes from "#table/table.routes"
+import tableRoutes from "#table/table.routes";
+import categoryRoutes from "#category/category.routes"
 
 router.use('/auth', authRoutes);
 router.use('/table', tableRoutes);
+router.use('/category', categoryRoutes);
 
 export default router;
