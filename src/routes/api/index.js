da3fc267994d6ade6dacc4803760d159bd750953
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-27 12:34:30 +0200

    fix(imports):TYPOs

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import authRoutes from "#auth/auth.routes";
-import tableRoutes from "#table/table.routes";
-import categoryRoutes from "#category/category.routes"
+import authRoutes from "#auth/auth.routes.js";
+import categoryRoutes from "#category/category.routes.js";
+import tableRoutes from "#table/table.routes.js";
 
 router.use('/auth', authRoutes);
 router.use('/table', tableRoutes);
