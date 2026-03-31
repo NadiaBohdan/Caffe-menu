7b85562cbbd0f4626e5a8905de14f392429f72e6
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-03-31 10:41:21 +0300

    feat(ssr): add ssr routes to main pages

--- src/modules/SSR/main/main.routes.js
+++ src/modules/SSR/main/main.routes.js
@@ -6,4 +6,28 @@ const router = express.Router();
 
 router.get('/', asyncCatch(mainSSRController.renderMainpage));
 
+router.get('/menu', asyncCatch());
+
+router.get('/menu/:id', asyncCatch());
+
+router.get('favourite', asyncCatch());
+
+router.get('/cart', asyncCatch());
+
+router.get('/contact', asyncCatch());
+
+router.get('/orders', asyncCatch());
+
+router.get('/orders/:id', asyncCatch());
+
+router.get('/checkout-order', asyncCatch());
+
+router.get('/login', asyncCatch());
+
+router.get('/login', asyncCatch());
+
+router.get('/register', asyncCatch());
+
+router.get('/account', asyncCatch());
+
 export default router;
