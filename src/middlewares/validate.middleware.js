28dcb3895a5ec41ffc4ebe6e51af3ee7b6493f7d
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-03 16:58:44 +0300

    refactor(middleware): add data existing check to validate methods

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,5 +1,9 @@
+import { ApiError } from "#utils/error.util.js";
+
 export const validateBody = (schema) => (req, res, next) => {
     try {
+        if(!req.body) throw new ApiError(400, "Invalid data");
+
         console.log("[INFO] Try validate: ", req.body)
         req.body = schema.parse(req.body);
         console.log("[INFO] Validate output: ", req.body)
@@ -11,6 +15,8 @@ export const validateBody = (schema) => (req, res, next) => {
 
 export const validateParams = (schema) => (req, res, next) => {
     try {
+        if(!req.params) throw new ApiError(400, "Invalid data");
+
         req.params = schema.parse(req.params);
         next()
     } catch(err) {
@@ -18,9 +24,13 @@ export const validateParams = (schema) => (req, res, next) => {
     }
 }
 
-export const validateFile = (schema) => (req, res, next) => {
+export const validateFile = (schema, isRequired) => (req, res, next) => {
     try {
-        if(!req.file) return next();
+        if(!req.file) {
+            if(isRequired) throw new ApiError(400, "Invalid data");
+
+            return next()
+        }
 
         req.file = schema.parse(req.file);
         next()
