fda0ca7114f7287e0a2addbb53913ed49b9761d1
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-03 15:36:27 +0300

    fix(dto): TYPO and add .default(0) to percentDiscount field

--- src/modules/product/product.dto.js
+++ src/modules/product/product.dto.js
@@ -5,7 +5,7 @@ const productCore = z.object({
     description: z.string().max(500, "Too long description"),
     price: z.coerce.number().positive("Price must be more than 0"),
     sortOrder: z.coerce.number().int().nonnegative("Invalid sort order"),
-    discount: z.coerce.number().nonnegative("Discount can't be less than 0").max(100, "Discount can't be more than 100"),
+    percentDiscount: z.coerce.number().nonnegative("Discount can't be less than 0").max(100, "Discount can't be more than 100").default(0),
     categoryId: z.coerce.number().int("Id must be integer"),
     isAvailable: z.preprocess(value => value === "true", z.boolean()).default(true),
     imgPath: z.string().url("Invalid image URL").nullable().optional()
@@ -15,7 +15,7 @@ export const productIdDto = z.object({
     id: z.coerce.number().int().positive()
 });
 
-export const createProductDto = productCore.omit({ isAvailable: true });
+export const createProductDto = productCore.omit({ isAvailable: true, sortOrder: true });
 
 export const updateProductDto = productCore.partial();
 
