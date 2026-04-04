2b62592b1fdef66da7e630d057430c1d21b986d5
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-04 21:24:43 +0300

    fix(favourite): fix req.user bug

--- src/modules/favourite/favourite.controller.js
+++ src/modules/favourite/favourite.controller.js
@@ -2,7 +2,7 @@ import { favouriteService } from "./favourite.service.js";
 
 export const favouriteController = {
     async toggle(req, res) {
-        await favouriteService.toggle(req.body);
+        await favouriteService.toggle({ ...req.body, userId: req.user.id });
 
         return res.status(200).json({
             success: true
--- src/modules/favourite/favourite.dto.js
+++ src/modules/favourite/favourite.dto.js
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 const favoriteCore = z.object({
-    userId: z.coerce.number().int().positive("Id must be bigger than 0"),
     productId: z.coerce.number().int().positive("Id must be bigger than 0")
 })
 
