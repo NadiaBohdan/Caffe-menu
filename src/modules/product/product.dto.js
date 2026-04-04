21d3116179fcbe7ba4f0ec9e305fb2eeccfd3af5
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-04 09:53:50 +0300

    feat(product): add amount colomn to db and dto

--- src/modules/product/product.dto.js
+++ src/modules/product/product.dto.js
@@ -4,6 +4,7 @@ const productCore = z.object({
     title: z.string().min(1, "Too short title"),
     description: z.string().max(500, "Too long description"),
     price: z.coerce.number().positive("Price must be more than 0"),
+    amount: z.coerce.number().int().positive("Amount must be more than 0"),
     sortOrder: z.coerce.number().int().nonnegative("Invalid sort order"),
     percentDiscount: z.coerce.number().nonnegative("Discount can't be less than 0").max(100, "Discount can't be more than 100").default(0),
     categoryId: z.coerce.number().int("Id must be integer"),
