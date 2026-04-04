6e0c3ed93d3ea6dec9949ab9eebc3bcb1072a64a
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-04 16:02:08 +0300

    feat(favourite): implement controller layer

---
+++ src/modules/favourite/favourite.controller.js
@@ -0,0 +1,12 @@
+import { favouriteService } from "./favourite.service.js";
+
+const favouriteController = {
+    async toggle(req, res) {
+        await favouriteService.toggle(req.body);
+        
+        return res.status(200).json({
+            message: "Product was added to favourites",
+            success: true
+        })
+    }
+}
