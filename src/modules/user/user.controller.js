5e814dae983e4cbf48263d79749aea10749858db
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-11 16:14:19 +0300

    feat(user): implement controller layer

---
+++ src/modules/user/user.controller.js
@@ -0,0 +1,17 @@
+import { userService } from "./user.service.js";
+
+export const userController = {
+    async update(req, res) {
+        const user = await userService.update({ ...req.body, ...req.params });
+        return res.status(200).json({
+            success: true,
+            message: "Updated uccessufully",
+            user
+        });
+    },
+
+    async delete(req, res) {
+        await userService.delete({ ...req.params });
+        return res.sendStatus(204);
+    }
+}
