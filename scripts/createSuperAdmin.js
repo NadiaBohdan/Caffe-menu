211c23688068573d254346c6e51d9989286e7214
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-19 11:52:42 +0300

    feat(scripts): add seed script for add super admin

---
+++ scripts/createSuperAdmin.js
@@ -0,0 +1,30 @@
+import { hashPassword } from "#utils/hash.util.js";
+import { staffRepository } from "#staff/staff.repository.js";
+import dotenv from "dotenv";
+import prisma from "#configs/prisma.js";
+
+dotenv.config();
+
+const createSuperAdmin = async () => {
+    try {
+        const login = process.env.SUPER_ADMIN_LOGIN
+        const password = await hashPassword(process.env.SUPER_ADMIN_PASSWORD);
+
+        const isExist = await staffRepository.getByLogin(login);
+
+        if(isExist) {
+            console.log("[ INFO ]: Super admin already exist. Skipping...");
+            return;
+        }
+
+        await staffRepository.create({ login, password, role: 'admin' });
+
+        console.log("[ INFO ]: Super admin created");
+    } catch(err) {
+        console.error("[ ERROR ]: Error craeting super admin: ", err);
+    } finally {
+        await prisma.
+    }
+}
+
+createSuperAdmin();
