bf9008c06ac9d04570252dcd8a7fbf7e2fa97f2c
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-20 16:07:14 +0300

    feat(admin ssr): implement controller layer

--- src/modules/SSR/admin/admin.controller.js
+++ src/modules/SSR/admin/admin.controller.js
@@ -1,3 +1,52 @@
-export const adminSSRController = {
+import { adminSSRservice } from "./admin.service.js";
 
-}
+const DIR = 'admin';
+
+const VIEWS = {
+    LOGIN: 'login',
+    CATEGORIES: 'categories',
+    PRODUCTS: 'products',
+    FOOTER: 'footer'
+};
+
+const renderAdmin = (res, view, data = {}) => {
+    return res.render(`${DIR}/${view}`, data);
+};
+
+export const adminSSRController = {
+    async renderLogin(req, res) {
+        renderAdmin(res, VIEWS.LOGIN, {
+            link: VIEWS.LOGIN
+        });
+    },
+
+    async renderCategories(req, res) {
+        const data = await adminSSRservice.getCategories(req.user.id);
+
+        renderAdmin(res, VIEWS.CATEGORIES, {
+            link: VIEWS.CATEGORIES,
+            categories: data.categories,
+            staff: data.staff
+        });
+    },
+
+    async renderProducts(req, res) {
+        const data = await adminSSRservice.getProducts(req.user.id);
+
+        renderAdmin(res, VIEWS.PRODUCTS, {
+            link: VIEWS.PRODUCTS,
+            products: data.products,
+            staff: data.staff
+        });
+    },
+
+    async renderFooter(req, res) {
+        const data = await adminSSRservice.getContacts(req.user.id)
+
+        renderAdmin(res, VIEWS.FOOTER, {
+            link: VIEWS.FOOTER,
+            contacts: data.contacts,
+            staff: data.staff
+        });
+    }
+};
