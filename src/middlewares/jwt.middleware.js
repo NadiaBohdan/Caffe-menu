ddc02a25128c70ef1b12ef8d49e8ddd0f4eda2f4
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-04-22 20:49:34 +0300

    feat(middleware): implement jwt validation ssr

--- src/middlewares/jwt.middleware.js
+++ src/middlewares/jwt.middleware.js
@@ -21,6 +21,26 @@ export const jwtValidate = async (req, res, next) => {
     }
 }
 
+export const jwtValidateSSR = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer ")) {
+            res.redirect('/login');
+        }
+        
+        const token = authHeader.split(" ")[1];
+
+        const decoded = await verifyToken(token);
+
+        req.user = { id: decoded.id, role: decoded.role };
+
+        next();
+    } catch(err) {
+        res.redirect('/login');
+    }
+}
+
 export const jwtOptional = async (req, res, next) => {
     const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
 
@@ -31,6 +51,7 @@ export const jwtOptional = async (req, res, next) => {
 
     try {
         const decoded = await verifyToken(token);
+
         req.user = decoded;
     } catch(err) {
         res.clearCookie("accessToken");
