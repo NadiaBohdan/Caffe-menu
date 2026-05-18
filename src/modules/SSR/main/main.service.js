16967c649bebbe7b6216d9148d59f3bff7ae3a80
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-05-18 23:10:03 +0300

    feat(menu): add isFacourite to product object

--- src/modules/SSR/main/main.service.js
+++ src/modules/SSR/main/main.service.js
@@ -28,9 +28,19 @@ export const mainSSRService = {
     async menu({ userId, id }) {
         const user = await getOptionalUserData(userId);
         const categories = await categoryService.getAll();
-        const products = await productService.getByCategory(id);
+        const rawProducts = await productService.getByCategory(id);
+        const favourites = await favouriteService.getAll({ userId })
         const contacts = await contactService.getAll();
         const activeCategory = id;
+
+        const favoritesId = new Set(favourites.map(fav => fav.id));
+
+        const products = rawProducts.map(product => {
+            return {
+                ...product,
+                isFavorite: favoritesId.has(product.id)
+            }
+        })
 
         return { user, products, categories, activeCategory, contacts };
     },
