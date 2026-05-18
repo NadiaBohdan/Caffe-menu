e6aefe3132889bbc2ce9562bfce504d5a14a4adc
Bohdan Saiko <bohdan.saiko.m@gmail.com>
2026-05-18 21:43:52 +0300

    fix(user): user update flow

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -2,7 +2,8 @@ import { userService } from "./user.service.js";
 
 export const userController = {
     async update(req, res) {
-        const user = await userService.update({ ...req.body, ...req.params });
+        console.log("DATA::::: ", { ...req.body, ...req.user })
+        const user = await userService.update({ ...req.body, ...req.user });
         return res.status(200).json({
             success: true,
             message: "Updated uccessufully",
