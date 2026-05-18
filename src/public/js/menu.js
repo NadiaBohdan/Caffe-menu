b81719a9119832a9f92910f24c3e20f3dd02d9ec
NadiaSuganiaka <suhaniaka.nadia@gmail.com>
2026-05-18 21:46:44 +0300

    Debuging

--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -1,61 +1,58 @@
-{
-    const heartsArray = document.querySelectorAll(".heart");
-    const cartArray = document.querySelectorAll(".cart");
+const heartsArray = document.querySelectorAll(".heart");
+const cartArray = document.querySelectorAll(".cart");
 
-    cartArray.addEventListener('click', async () => {
-        try{
-            const respons = await fetch('', {
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    "cartId": 2,
-                    productId: Number(productId),
-                    "quantity": 1
-                })
-            })
-            const data = await respons.json();
-
-            if (!respons.ok) {
-                alert(data.message || "Any error")
-                return;
-            }
-        }catch(err){
-            alert("Backend besprizornic:" + err);
-        }
-    })
-
-
-    heartsArray.forEach(heart => {
-        const path = heart.querySelector('path'); // беремо path всередині svg
-
-        heart.addEventListener('click', async () => {
-            const productId = heart.closest('[data-product-id]').dataset.productId;
-
-            if (path.getAttribute('fill') !== '#d35252') {
-                path.setAttribute('fill', '#d35252');
-
-                await fetch('http://localhost:3000/api/favourite/toggle', {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ productId: Number(productId) })
-                });
-
-            } else {
-                path.setAttribute('fill', '#ffffff');
-
-                
-            }
-        });
-    });
-}
-/*
-hearts.addEventListener('click', async () => {
+/*cartArray.addEventListener('click', async () => {
     try{
+        const respons = await fetch('', {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "cartId": 2,
+                productId: Number(productId),
+                "quantity": 1
+            })
+        })
+        const data = await respons.json();
 
-
+        if (!respons.ok) {
+            alert(data.message || "Any error")
+            return;
+        }
     }catch(err){
         alert("Backend besprizornic:" + err);
     }
+})*/
+
+
+heartsArray.forEach(heart => {
+    const path = heart.querySelector('path'); // беремо path всередині svg
+
+    heart.addEventListener('click', async () => {
+        const productId = heart.closest('[data-product-id]').dataset.productId;
+
+        if (path.getAttribute('fill') !== '#d35252') {
+            path.setAttribute('fill', '#d35252');
+
+        } else {
+            path.setAttribute('fill', '#ffffff');
+        }
+
+        await fetch('http://localhost:3000/api/favourite/toggle', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ productId: Number(productId) })
+        });
+    });
+});
+
+/*
+hearts.addEventListener('click', async () => {
+try{
+
+
+}catch(err){
+    alert("Backend besprizornic:" + err);
+}
 });*/
