890a7f34fd1cde8da3cc58a53fc2c66abbc80f90
NadiaSuganiaka <suhaniaka.nadia@gmail.com>
2026-05-21 00:51:40 +0300

    start page of categories

---
+++ src/public/js/sidebar.js
@@ -0,0 +1,5 @@
+/*document.querySelectorAll('.sidebar-categor').forEach(link => {
+    if (link.getAttribute('href') === window.location.pathname) {
+        link.classList.add('active');
+    }
+});*/
